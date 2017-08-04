49ac7b7396decb5db25362f558e15eb524b50cd7
Rich Cross <richwesley@comcast.net>
2017-08-04 12:50:56 -0700

    added html routes,  roadtrip db, index.htm,

--- models/itenerary.js
+++ models/itenerary.js
@@ -1,1 +1,11 @@
-itinerary
+module.exports = function(sequelize, DataTypes) {
+  var Itenerary = sequelize.define("Itenerary", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+      }
+     })
+    return Itenerary;
+ };
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const path = require("path");
+const app = express();
+ 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+const db = require("./models");
+
+// Routes
+// =============================================================
+//require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+// Syncing sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+      });
+});
