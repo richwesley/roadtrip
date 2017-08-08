95a8433c236fd2e7d6a84cf012de097de021a453
Rich Cross <richwesley@comcast.net>
2017-08-07 19:00:39 -0700

    spelling errors

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,8 +15,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/user.html"));
   });
 
-  app.get("/itenerary", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/itenerary.html"));
+  app.get("/itinerary", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/itinerary.html"));
   });
 
   // If no matching route is found default to home
