e99c7c50c663cae49ed005bbd2f0685b29464076
Rich Cross <richwesley@comcast.net>
2017-08-09 18:08:32 -0700

    create user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    // Giving the Author model a name of type STRING
+   
     username:{
       type: DataTypes.STRING,
       allowNull: false
@@ -24,8 +24,7 @@ module.exports = function(sequelize, DataTypes) {
   });
 
   User.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
+   
     User.hasMany(models.Itinerary, {
       onDelete: "cascade"
     });
